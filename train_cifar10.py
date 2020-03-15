1c60e1b6b4567bd7879c42dfc501505688c96275
kallol-oto <kallolsamanta.pcmt@gmail.com>
2020-03-15 19:43:26 +0530

    training with cyclical learning rate

--- train_cifar10.py
+++ train_cifar10.py
@@ -3,12 +3,13 @@
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
+
 matplotlib.use("Agg")
 
 # import the necessary packages
-from pyimagesearch.minigooglenet import MiniGoogLeNet
-from pyimagesearch.clr_callback import CyclicLR
-from pyimagesearch import config
+from minigooglenet import MiniGoogLeNet
+from clr_callback import CyclicLR
+import config
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from keras.preprocessing.image import ImageDataGenerator
@@ -36,39 +37,39 @@ testY = lb.transform(testY)
 
 # construct the image generator for data augmentation
 aug = ImageDataGenerator(width_shift_range=0.1,
-	height_shift_range=0.1, horizontal_flip=True,
-	fill_mode="nearest")
+                         height_shift_range=0.1, horizontal_flip=True,
+                         fill_mode="nearest")
 
 # initialize the optimizer and model
 print("[INFO] compiling model...")
 opt = SGD(lr=config.MIN_LR, momentum=0.9)
 model = MiniGoogLeNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # initialize the cyclical learning rate callback
 print("[INFO] using '{}' method".format(config.CLR_METHOD))
 clr = CyclicLR(
-	mode=config.CLR_METHOD,
-	base_lr=config.MIN_LR,
-	max_lr=config.MAX_LR,
-	step_size= config.STEP_SIZE * (trainX.shape[0] // config.BATCH_SIZE))
+    mode=config.CLR_METHOD,
+    base_lr=config.MIN_LR,
+    max_lr=config.MAX_LR,
+    step_size=config.STEP_SIZE * (trainX.shape[0] // config.BATCH_SIZE))
 
 # train the network
 print("[INFO] training network...")
 H = model.fit_generator(
-	aug.flow(trainX, trainY, batch_size=config.BATCH_SIZE),
-	validation_data=(testX, testY),
-	steps_per_epoch=trainX.shape[0] // config.BATCH_SIZE,
-	epochs=config.NUM_EPOCHS,
-	callbacks=[clr],
-	verbose=1)
+    aug.flow(trainX, trainY, batch_size=config.BATCH_SIZE),
+    validation_data=(testX, testY),
+    steps_per_epoch=trainX.shape[0] // config.BATCH_SIZE,
+    epochs=config.NUM_EPOCHS,
+    callbacks=[clr],
+    verbose=1)
 
 # evaluate the network and show a classification report
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=config.BATCH_SIZE)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=config.CLASSES))
+                            predictions.argmax(axis=1), target_names=config.CLASSES))
 
 # construct a plot that plots and saves the training history
 N = np.arange(0, config.NUM_EPOCHS)
@@ -76,8 +77,8 @@ plt.style.use("ggplot")
 plt.figure()
 plt.plot(N, H.history["loss"], label="train_loss")
 plt.plot(N, H.history["val_loss"], label="val_loss")
-plt.plot(N, H.history["acc"], label="train_acc")
-plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.plot(N, H.history["accuracy"], label="train_acc")
+plt.plot(N, H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
