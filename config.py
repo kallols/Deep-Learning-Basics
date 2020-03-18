0f48ac97bfefd1c871db677d52315d3ddaa6107a
kallol-oto <kallolsamanta.pcmt@gmail.com>
2020-03-18 16:00:39 +0530

    parameters updated

--- config.py
+++ config.py
@@ -3,18 +3,19 @@ import os
 
 # initialize the list of class label names
 CLASSES = ["airplane", "automobile", "bird", "cat", "deer", "dog",
-	"frog", "horse", "ship", "truck"]
+           "frog", "horse", "ship", "truck"]
 
 # define the minimum learning rate, maximum learning rate, batch size,
 # step size, CLR method, and number of epochs
-MIN_LR = 1e-7
+MIN_LR = 1e-5
 MAX_LR = 1e-2
 BATCH_SIZE = 64
 STEP_SIZE = 8
 CLR_METHOD = "triangular"
-NUM_EPOCHS = 96
+NUM_EPOCHS = 48
 
 # define the path to the output training history plot and cyclical
 # learning rate plot
-TRAINING_PLOT_PATH = os.path.sep.join(["output", "training_plot.png"])
-CLR_PLOT_PATH = os.path.sep.join(["output", "clr_plot.png"])
+LRFIND_PLOT_PATH = os.path.sep.join(["/home/oto/PycharmProjects/deep_learning", "lrfind_plot.png"])
+TRAINING_PLOT_PATH = os.path.sep.join(["/home/oto/PycharmProjects/deep_learning", "training_plot.png"])
+CLR_PLOT_PATH = os.path.sep.join(["/home/oto/PycharmProjects/deep_learning", "clr_plot.png"])
